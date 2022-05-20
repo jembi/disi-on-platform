782a44ed8d63ba1deab4dd9ebcc4c45ed582c704
bradsawadye <bradsawadye@gmail.com>
2022-05-20 13:55:57 +0200

    Extract the identity domains into a file

--- importer/santempi/santeMpiConfig.js
+++ importer/santempi/santeMpiConfig.js
@@ -170,33 +170,9 @@ const createClientDomain = (accessToken, domainUrl, domainName, domainId, client
 const createClientDomains = async accessToken => {
   const promises = []
 
-  const domains = [
-    {
-      url: 'https://instantopenhie.org/client1',
-      id: 'DF5BEC1E-8C41-4FF1-8E65-A39AC1DDAE60',
-      name: 'DISI OpenHie Client1'
-    },
-    {
-      url: 'http://openhie.org/fhir/hiv-casereporting/identifier/art',
-      id: 'DF5BEC1E-8C41-4FF1-8E65-A39AC1DDAE61',
-      name: 'DISI OpenHie HIV Reporting ART'
-    },
-    {
-      url: 'http://openhie.org/fhir/hiv-casereporting/identifier/passport',
-      id: 'DF5BEC1E-8C41-4FF1-8E65-A39AC1DDAE62',
-      name: 'DISI OpenHie HIV Passport'
-    },
-    {
-      url: 'http://openhie.org/fhir/hiv-casereporting/identifier/nid',
-      id: 'DF5BEC1E-8C41-4FF1-8E65-A39AC1DDAE63',
-      name: 'DISI OpenHie HIV NID'
-    },
-    {
-      url: 'http://openhie.org/fhir/hiv-casereporting/identifier/facility1',
-      id: 'DF5BEC1E-8C41-4FF1-8E65-A39AC1DDAE64',
-      name: 'DISI OpenHie HIV Facility'
-    }
-  ]
+  const domains = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, 'identityDomains.json'))
+  ).domains
 
   domains.forEach(domain => {
     promises.push(
