194b7980b4a6a5a930935374936bf6f6d0312bf8
michaelloosen <thorne199@gmail.com>
2022-07-18 09:45:29 +0200

    Update deploy scripts to use new repo name To ensure they work

--- deploy-test.sh
+++ deploy-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #TODO: remove the instant version once the config importer bug is fixed in the Platform
-./platform-linux "$1" -c="../disi-hiv-poc" --env-file="./.env.test" --instant-version="michael-disi" disi-poc
+./platform-linux "$1" -c="../disi-on-platform" --env-file="./.env.test" --instant-version="michael-disi" disi-poc
