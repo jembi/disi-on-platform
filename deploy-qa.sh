bacb95ddd213c26b1099d9c7703ec83da8306d00
michaelloosen <thorne199@gmail.com>
2022-09-06 11:48:46 +0200

    Rename all references to disi-poc To use the new disi-on-platform name

--- deploy-qa.sh
+++ deploy-qa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #TODO: remove the instant version once the config importer bug is fixed in the Platform
-./platform-linux "$1" -c="../disi-on-platform" --env-file="./.env.qa" --instant-version="michael-disi" disi-poc
+./platform-linux "$1" -c="../disi-on-platform" --env-file="./.env.qa" --instant-version="michael-disi" disi-on-platform
